290c4db66cee85db859dcbbc3f27b2ae9039e481
정지원 <4dakto@gmail.com>
2017-10-09 16:57:56 +0900

    Jiwon ::  add SagaMonitor

--- src/store.js
+++ src/store.js
@@ -2,12 +2,13 @@ import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 // import thunk from 'redux-thunk'
 // import logger from 'redux-logger';
+import sagaMonitor from './lib/sagaMoniter';
 import reducers from './reducers/rootReducer'
 
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './sagas/sagaIndex'
 
-const sagaMiddleware = createSagaMiddleware()
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor })
 
 export default createStore(
   reducers,
