072ebc04a2d6a02e497bef5f86844657c04eb293
Jiwon Jung <jw.jeoung@albamapp.com>
2017-11-03 11:51:51 +0900

    Jiwon:: add ramda

--- src/service/__tests__/postService.spec.js
+++ src/service/__tests__/postService.spec.js
@@ -1,26 +1,40 @@
+import R from 'ramda';
 import * as uut from '../postService';
 global.fetch = require('jest-fetch-mock');
 
 describe.only('service/postService', () => {
-
   beforeEach(() => {
-    jest.resetAllMocks()
-  })
-  const data = [ { id: 158994,
-    title: 'qwerqwer',
-    categories: '분류 없음',
-    content: 'qwerqwer' },
-    { id: 158993,
+    jest.resetAllMocks();
+  });
+  const data = [
+    {
+      id: 158994,
+      title: 'qwerqwer',
+      categories: '분류 없음',
+      content: 'qwerqwer'
+    },
+    {
+      id: 158993,
       title: 'werwqer',
       categories: 'test',
-      content: 'qwerqwer' },
-    { id: 158114,
+      content: 'qwerqwer'
+    },
+    {
+      id: 158114,
       title: '헐 다지워짐..-_-',
       categories: 'test',
-      content: 'ㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㄹ' } ]
+      content: 'ㅁㄴㅇㄹㅁㄴㅇㄹㅁㄴㅇㄹ'
+    }
+  ];
   it('초기 값을 fetch해야 한다', async () => {
-    fetch.mockResponseOnce(JSON.stringify(data))
-    const response = await uut.getPosts()
-    expect(response.data).toEqual(data)
-  })
-})
+    const response = await uut.getPosts();
+    const responseData = response.data
+    const hasIdInProps = R.compose(
+      R.map(R.prop('id')),
+      R.type
+    )(data)
+    const checkType = R.type
+    const pred = R.where({id: R.equals(R.type, "Number")})
+    expect(pred(responseData)).toEqual(true);
+  });
+});
